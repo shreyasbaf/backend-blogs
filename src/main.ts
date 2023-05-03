0109835a693e13bbdae5a75570683e0b3c55acda
shreyasbaf <shreyas7bafna@gmail.com>
2023-05-03 16:05:56 +0530

    fix: hostname

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const port = process.env.PORT || 3000;
-  const host = process.env.HOST || 'localhost';
-  // await app.listen(3000);
-  await app.listen(port, host);
-  console.log(`Server running on http://${host}:${port}`);
+  // const port = process.env.PORT || 3000;
+  // const host = process.env.HOST || 'localhost';
+  await app.listen(3000);
+  // await app.listen(port, host);
+  // console.log(`Server running on http://${host}:${port}`);
 }
 bootstrap();
